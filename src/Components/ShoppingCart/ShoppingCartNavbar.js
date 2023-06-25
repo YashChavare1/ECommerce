6f2d227a575c55507de2464988038ba612daa8f6
Yash <yashchavare1@gmail.com>
2023-06-25 16:32:20 +0530

    add backbutton function

--- src/Components/ShoppingCart/ShoppingCartNavbar.js
+++ src/Components/ShoppingCart/ShoppingCartNavbar.js
@@ -1,13 +1,20 @@
+import { useNavigate } from 'react-router-dom';
 import backLogo from '../../images/back-arrow.png';
 import cartLogo from '../../images/cart icon.png';
 import './ShoppingCartNavbar.css';
 
 function ShoppingCartNavbar() {
+
+    const nav = useNavigate();
+
+    const handleBackBtnClicked = () => {
+        nav('/');
+    }
+
     return(
         <>
             <div id="navbar">
-                <img id='backLogo' src={ backLogo } alt='Back Button' />
-                {/* <p id='text'> Shopping Cart </p> */}
+                <img id='backLogo' src={ backLogo } alt='Back Button' onClick={ handleBackBtnClicked } />
                 <img id='cartLogo' src={ cartLogo } alt='Cart Logo' />
             </div>
         </>
